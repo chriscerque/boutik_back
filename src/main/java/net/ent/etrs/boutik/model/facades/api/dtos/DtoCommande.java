ce6b0b933d8c8c12c768fdc230d0418416ebd4cb
chriscerque <christophe.cerqueira741@gmail.com>
2022-10-11 08:51:09 +0200

    Initial commit

---
+++ src/main/java/net/ent/etrs/boutik/model/facades/api/dtos/DtoCommande.java
@@ -0,0 +1,22 @@
+package net.ent.etrs.boutik.model.facades.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoCommande {
+
+    private Long id;
+    private Long user;
+    private String etat;
+    private String createdAt;
+    private Map<Long, Integer> produits = new HashMap<>();
+}
